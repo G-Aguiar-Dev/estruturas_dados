a55646cc9117a07dacd3976d8ce9dc75715433ff
Guilherme Aguiar <202311190034@ifba.edu.br>
2025-10-20 22:35:48 -0300

    feat: fila dinâmica + BookDAO

---
+++ Fila Dupla Terminação Dinâmica/demo/src/main/java/com/example/Queueable.java
@@ -0,0 +1,16 @@
+public interface Queueable<T>{
+    
+    // Métodos Principais
+    void BeginEnqueue(T data);
+    void EndEnqueue(T data);
+    T BeginDequeue();
+    T EndDequeue();
+    T front();
+    T rear();
+
+    // Métodos Auxiliares
+    boolean isEmpty();
+    boolean isFull();
+    String printFrontToRear();
+    String printRearToFront();
+}
